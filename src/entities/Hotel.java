842158c9ea08d38bdeda5f86be3f83fb92908a16
atsybul <ua.ants@gmail.com>
2016-11-03 20:48:54 +0200

    Andrii Tsybulskyi: GoIT Java Core Final. Initial skeleton.

---
+++ src/entities/Hotel.java
@@ -0,0 +1,11 @@
+package entities;
+
+import java.util.List;
+
+public class Hotel {
+    private long hotelId;
+    private String city;
+    private String name;
+    private List<Room> rooms;
+
+}
---
+++ src/entities/Room.java
@@ -0,0 +1,9 @@
+package entities;
+
+public class Room {
+    private long roomId;
+    private User userReserved;
+    private int persons;
+    private int price;
+    private Hotel hotel;
+}
