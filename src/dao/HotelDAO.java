446eda824ef14532c6918d3173cd7508fa066f02
atsybul <ua.ants@gmail.com>
2016-11-06 23:47:58 +0200

    Andrii Tsybulskyi: Added HotelDAO

---
+++ src/dao/HotelDAO.java
@@ -0,0 +1,31 @@
+package dao;
+
+import entities.Hotel;
+import java.util.List;
+
+public class HotelDAO implements AbstractDAO<Hotel> {
+    @Override
+    public Hotel save(Hotel hotel) {
+        return null;
+    }
+
+    @Override
+    public void delete(Hotel hotel) {
+
+    }
+
+    @Override
+    public void deleteAll(List<Hotel> hotels) {
+
+    }
+
+    @Override
+    public void saveAll(List<Hotel> hotels) {
+
+    }
+
+    @Override
+    public List<Hotel> getList() {
+        return null;
+    }
+}
