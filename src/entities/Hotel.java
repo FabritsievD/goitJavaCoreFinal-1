444efc8abdc12b38eeb2a488d992eef8a757fb7e
atsybul <ua.ants@gmail.com>
2016-11-06 23:25:15 +0200

    Andrii Tsybulskyi: Add Hotel constructors and getters

--- src/entities/Hotel.java
+++ src/entities/Hotel.java
@@ -8,4 +8,26 @@ public class Hotel {
     private String name;
     private List<Room> rooms;
 
+    public Hotel(long hotelId, String city, String name, List<Room> rooms) {
+        this.hotelId = hotelId;
+        this.city = city;
+        this.name = name;
+        this.rooms = rooms;
+    }
+
+    public long getHotelId() {
+        return hotelId;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Room> getRooms() {
+        return rooms;
+    }
 }
