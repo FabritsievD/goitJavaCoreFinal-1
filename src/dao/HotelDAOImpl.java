448d2c946c1d2682005df0f08b6a1c60af949178
atsybul <ua.ants@gmail.com>
2016-11-07 01:07:07 +0200

    Andrii Tsybulskyi: Implemented methods for HotelDAOImpl

--- src/dao/HotelDAOImpl.java
+++ src/dao/HotelDAOImpl.java
@@ -1,31 +1,72 @@
 package dao;
 
 import entities.Hotel;
+
+import java.io.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class HotelDAOImpl implements AbstractDAO<Hotel> {
+
+    List<Hotel> hotels = new ArrayList<>();
+    File fileHotels = new File("fileHotels.bin");
+    ObjectOutputStream hotelOs;
+    ObjectInputStream hotelIs;
+
+
     @Override
     public Hotel save(Hotel hotel) {
-        return null;
+        hotels = getList();
+        if(!hotels.contains(hotel)) {
+            hotels.add(hotel);
+            saveAll(hotels);
+        }
+        return hotel;
     }
 
     @Override
     public void delete(Hotel hotel) {
+        hotels = getList();
+        if(hotels.contains(hotel)) {
+            hotels.remove(hotel);
+            saveAll(hotels);
+        }
 
     }
 
     @Override
     public void deleteAll(List<Hotel> hotels) {
-
+        this.hotels.removeAll(hotels);
+        saveAll(this.hotels);
     }
 
     @Override
     public void saveAll(List<Hotel> hotels) {
+        this.hotels = hotels;
+        try {
+            hotelOs = new ObjectOutputStream(new FileOutputStream(fileHotels));
+            hotelOs.writeObject(hotels);
+            hotelOs.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            System.err.println("File not found");
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.err.println("IO Exception");
+        }
 
     }
 
     @Override
     public List<Hotel> getList() {
-        return null;
+        try {
+            hotelIs = new ObjectInputStream(new FileInputStream(fileHotels));
+            hotels = (List<Hotel>) hotelIs.readObject();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return hotels;
     }
 }
--- src/entities/Hotel.java
+++ src/entities/Hotel.java
@@ -1,8 +1,9 @@
 package entities;
 
+import java.io.Serializable;
 import java.util.List;
 
-public class Hotel {
+public class Hotel implements Serializable{
     private long hotelId;
     private String city;
     private String name;
@@ -35,5 +36,37 @@ public class Hotel {
 
     public int getStars() {
         return stars;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Hotel hotel = (Hotel) o;
+
+        if (hotelId != hotel.hotelId) return false;
+        if (!city.equals(hotel.city)) return false;
+        return name.equals(hotel.name);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (int) (hotelId ^ (hotelId >>> 32));
+        result = 31 * result + city.hashCode();
+        result = 31 * result + name.hashCode();
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Hotel{" +
+                "hotelId=" + hotelId +
+                ", city='" + city + '\'' +
+                ", name='" + name + '\'' +
+                ", stars=" + stars +
+                ", rooms=" + rooms +
+                '}';
     }
 }
