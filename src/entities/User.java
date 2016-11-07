0cbee5263c6d306f5e0eca2f2747ca3e7335737b
atsybul <ua.ants@gmail.com>
2016-11-07 16:13:40 +0200

    Andrii Tsybulskyi: Added User class implementation

--- src/entities/User.java
+++ src/entities/User.java
@@ -1,7 +1,55 @@
 package entities;
 
-public class User {
-    long userId;
-    String firstName;
-    String lastName;
+import java.io.Serializable;
+
+public class User implements Serializable{
+    private long userId;
+    private String firstName;
+    private String lastName;
+
+    public User(long userId, String firstName, String lastName) {
+        this.userId = userId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        if (userId != user.userId) return false;
+        if (!firstName.equals(user.firstName)) return false;
+        return lastName.equals(user.lastName);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (int) (userId ^ (userId >>> 32));
+        result = 31 * result + firstName.hashCode();
+        result = 31 * result + lastName.hashCode();
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId=" + userId +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
 }
