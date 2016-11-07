8b4b2ea13ab9eb4e13727978d3966a48702926b5
fabritsievD <fabritsiev_dv@ukr.net>
2016-11-07 11:28:15 +0200

    1

--- src/entities/Room.java
+++ src/entities/Room.java
@@ -1,9 +1,93 @@
 package entities;
 
+import java.util.Date;
+
 public class Room {
-    private long roomId;
-    private User userReserved;
-    private int persons;
-    private int price;
-    private Hotel hotel;
+    private Long roomId;
+    private Integer persons;
+    private Integer price;
+    private String hotel;
+    private String city;
+    private boolean userReserved;
+    private Date dateAvalibleFrom;
+
+    public Room() {
+    }
+
+    public Room(long roomId, int persons, int price, String hotel, String city, boolean userReserved, Date dateAvalibleFrom) {
+        this.roomId = roomId;
+        this.persons = persons;
+        this.price = price;
+        this.hotel = hotel;
+        this.city = city;
+        this.userReserved = userReserved;
+        this.dateAvalibleFrom = dateAvalibleFrom;
+    }
+
+    public void setRoomId(long roomId) {
+        this.roomId = roomId;
+    }
+
+    public void setPersons(int persons) {
+        this.persons = persons;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public void setHotel(String hotel) {
+        this.hotel = hotel;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setDateAvalibleFrom(Date dateAvalibleFrom) {
+        this.dateAvalibleFrom = dateAvalibleFrom;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Room room = (Room) o;
+
+        if (roomId != null ? !roomId.equals(room.roomId) : room.roomId == null) return false;
+        if (persons != null ? !persons.equals(room.persons) : room.persons == null) return false;
+        if (price != null ? !price.equals(room.price) : room.price == null) return false;
+        if (hotel != null ? !hotel.equals(room.hotel) : room.hotel == null) return false;
+        if (dateAvalibleFrom != null ? !(dateAvalibleFrom.getTime()>=room.dateAvalibleFrom.getTime()) : room.dateAvalibleFrom ==null )return false;
+        return city != null ? city.equals(room.city) : room.city != null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = roomId != null ? roomId.hashCode() : 0;
+        result = 31 * result + (persons != null ? persons.hashCode() : 0);
+        result = 31 * result + (price != null ? price.hashCode() : 0);
+        result = 31 * result + (hotel != null ? hotel.hashCode() : 0);
+        result = 31 * result + (city != null ? city.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Room{" +
+                "roomId=" + roomId +
+                ", persons=" + persons +
+                ", price=" + price +
+                ", hotel='" + hotel + '\'' +
+                ", city='" + city + '\'' +
+                ", userReserved=" + userReserved +
+                ", dateAvalibleFrom=" + dateAvalibleFrom +
+                '}';
+    }
 }
---
+++ src/models/ApiImpl.java
@@ -0,0 +1,95 @@
+package models;
+
+
+import entities.Room;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+public class ApiImpl implements API {
+
+
+
+    @Override
+    public void bookRoom(long roomId, long userId, long hotelId) {
+
+    }
+
+    @Override
+    public void cancelReservation(long roomId, long userId, long hotelId) {
+
+    }
+
+    @Override
+    public HashSet<Room> findRoom(Map<String, String> params) {
+
+
+        Set<Room> rooms = new HashSet<>();
+        rooms.add( new Room(85236, 2, 700,"Hotel4","City1",true, new Date("2016/10/16")));
+        rooms.add( new Room(95423, 6,300 ,"Hotel5","City2",true, new Date("2016/9/16")));
+        rooms.add( new Room(45796, 4, 800,"Hotel1","City3",true, new Date("2016/10/16")));
+        rooms.add( new Room(49654, 1,200 ,"Hotel2","City4",true, new Date("2016/9/16")));
+        rooms.add( new Room(89134, 3, 600,"Hotel4","City1",true, new Date("2016/10/16")));
+        rooms.add( new Room(87563, 5,300 ,"Hotel3","City3",true, new Date("2016/9/16")));
+
+        DataBase db= new DataBase(rooms);
+
+        Room neededRoom = new Room();
+        HashSet<Room> findRooms = new HashSet<>();
+        if(params.containsKey("City")) {
+            neededRoom.setCity(params.get("City"));
+        }
+        if(params.containsKey("Hotel")){
+           neededRoom.setHotel(params.get("Hotel"));
+        }
+        if(params.containsKey("Price")){
+            try{
+            neededRoom.setPrice(Integer.valueOf(params.get("Price")));
+            }
+            catch (NumberFormatException e){
+                System.out.println("Wrong price");
+            }
+        }
+        if(params.containsKey("Persons")){
+            try{
+                neededRoom.setPersons(Integer.valueOf(params.get("Persons")));
+            }
+            catch (NumberFormatException e){
+                System.out.println("Wrong persons");
+            }
+        }
+        if(params.containsKey("Id")){
+            try{
+                neededRoom.setRoomId(Long.valueOf(params.get("Id")));
+            }
+            catch (NumberFormatException e){
+                System.out.println("Wrong Id");
+            }
+        }
+        if(params.containsKey("From")){
+
+                String tmp = params.get("From");
+                SimpleDateFormat format = new SimpleDateFormat();
+                format.applyPattern("dd.MM.yyyy");
+               try{
+                   Date date =format.parse(tmp);
+                   neededRoom.setDateAvalibleFrom(date);
+               }
+               catch (ParseException e){
+                   System.out.println("Wrong date format");
+               }
+
+
+        }
+
+        Iterator<Room> iterator =  db.getRooms().iterator();
+        while (iterator.hasNext()){
+            Room tmp= iterator.next();
+            if(neededRoom.equals(tmp)){
+                findRooms.add(tmp);
+            }
+        }
+        return findRooms;
+    }
+}
